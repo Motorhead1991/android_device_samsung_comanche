c72c6119fc5cd5bb4839ba705e9be559e21d04e5
Mike C <mcrosson_git at nusku dot net>
2012-12-08 04:18:17 +0000

    Add more overrides

--- cm.mk
+++ cm.mk
@@ -1,4 +1,5 @@
 $(call inherit-product, device/samsung/comanche/full_comanche.mk)
+$(call inherit-product, $(LOCAL_PATH)/build_props.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/gsm.mk)
@@ -10,6 +11,9 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-I547 TARGET_DEVICE=comanche BUILD_FINGERPRINT="SGH-I547-user 4.0.4 IMM76D I547UCALEM release-keys" PRIVATE_BUILD_DESC="samsung/SGH-I547/comanche:4.0.4/IMM76D/I547UCALEM:user/release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += USER=android-build
+PRODUCT_BUILD_PROP_OVERRIDES += KBUILD_BUILD_USER=android-build
+PRODUCT_BUILD_PROP_OVERRIDES += KBUILD_BUILD_HOST=buildbot
 
 PRODUCT_NAME := cm_comanche
 PRODUCT_DEVICE := comanche
