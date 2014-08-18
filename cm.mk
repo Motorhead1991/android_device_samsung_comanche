5170a4bb4e4e1ade355090369ee62e8e2bb50674
Motorhead1991 <Motor-head-1991@hotmail.com>
2014-08-18 00:22:43 -0700

    Update fingerprint to reflect proper device information

--- cm.mk
+++ cm.mk
@@ -10,7 +10,7 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=comanche TARGET_DEVICE=comanche BUILD_FINGERPRINT="amsung/apexqtmo/comanche:4.1.2/JZO54K/T699UVBMC5:user/release-keys" PRIVATE_BUILD_DESC="comanche-user 4.1.2 JZO54K T699UVBMC5 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=comanche TARGET_DEVICE=comanche BUILD_FINGERPRINT="Samsung/COMANCHE/comanche:4.1.2/JZO54K/I847UCCMH5:user/release-keys" PRIVATE_BUILD_DESC="comanche-user 4.1.2 JZO54K I847UCCMH5 release-keys"
 
 PRODUCT_NAME := cm_comanche
 PRODUCT_DEVICE := comanche
