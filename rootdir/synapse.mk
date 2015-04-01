e2460a4ae7be276cf8ec57af751f5c29ba469b73
Anthony Cox <motorhead1991@hotmail.com>
2015-04-01 04:26:18 -0600

    synapse: add device file and organize synapse.mk

--- rootdir/synapse.mk
+++ rootdir/synapse.mk
@@ -1,5 +1,9 @@
-# Synapse action files
+# Synapse Device
 PRODUCT_COPY_FILES := \
+    $(LOCAL_PATH)/synapse/device/.device.sh:root/res/synapse/.device.sh \
+
+# Synapse Actions
+PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/synapse/actions/boolean:root/res/synapse/actions/boolean \
     $(LOCAL_PATH)/synapse/actions/bracket-option:root/res/synapse/actions/bracket-option \
     $(LOCAL_PATH)/synapse/actions/buildprop:root/res/synapse/actions/buildprop \
@@ -28,9 +32,15 @@ PRODUCT_COPY_FILES := \
     $(LOCAL_PATH)/synapse/actions/voltage:root/res/synapse/actions/voltage \
     $(LOCAL_PATH)/synapse/actions/wake:root/res/synapse/actions/wake \
     $(LOCAL_PATH)/synapse/actions/zram:root/res/synapse/actions/zram \
+
+# Synapse Debug
+PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/synapse/debug/last_kmsg:root/res/synapse/debug/last_kmsg \
     $(LOCAL_PATH)/synapse/debug/pvs_bin:root/res/synapse/debug/pvs_bin \
     $(LOCAL_PATH)/synapse/debug/speed_bin:root/res/synapse/debug/speed_bin \
+
+# Synapse Files
+PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/synapse/files/bacon.sh:root/res/synapse/files/bacon.sh \
     $(LOCAL_PATH)/synapse/files/bck_prof:root/res/synapse/files/bck_prof \
     $(LOCAL_PATH)/synapse/files/dropcaches_prof:root/res/synapse/files/dropcaches_prof \
@@ -43,6 +53,9 @@ PRODUCT_COPY_FILES := \
     $(LOCAL_PATH)/synapse/files/speaker_prof:root/res/synapse/files/speaker_prof \
     $(LOCAL_PATH)/synapse/files/volt_prof:root/res/synapse/files/volt_prof \
     $(LOCAL_PATH)/synapse/files/wake_prof:root/res/synapse/files/wake_prof \
+
+# Synapse Config
+PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/synapse/config.json.generate:root/res/synapse/config.json.generate \
     $(LOCAL_PATH)/synapse/config.json.generate.adv:root/res/synapse/config.json.generate.adv \
     $(LOCAL_PATH)/synapse/config.json.generate.bprop:root/res/synapse/config.json.generate.bprop \
